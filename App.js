539258ec536144fb1377b92465f66f8bb38261b3
ss2261 <ss2261@kent.ac.uk>
2021-03-21 17:42:33 +0000

    status bar added for IOS but not on andriod view

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform } from 'react-native';
+
+
 
 export default function App() {
+  const statusbar = (Platform.OS=='ios') ? <View style={styles.statusBar}></View> : <View></View> //if on iphone then show status bar else show regular view
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {statusbar}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = StyleSheet.create({ //this is a class which is passed
+
+  constructor() { //constructor - as soon as the componet is rendered, it will run any code within it
+
+this.state= {
+  todoInput: '',
+  todosArr: [
+    {id:0, title: 'Code', done:true},
+    {id:1, title: 'Eat', done:false},
+  ]
+}    
   },
+  container: {
+    backgroundColor: 'white',
+  },
+  statusBar:{
+    backgroundColor: '#fc7753', //outraeous orange colour
+    height:50
+  }
 });
