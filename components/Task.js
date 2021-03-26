42128f7b3da5743f5df65ce11bc7fe5a60c6c93b
ss2261 <ss2261@kent.ac.uk>
2021-03-26 11:02:51 +0000

    working on completing and uncompleting tasks

--- components/Task.js
+++ components/Task.js
@@ -1,21 +1,45 @@
 import React, { useState } from 'react';
-import {StyleSheet, Text, TouchableOpacity, View, } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View, } from 'react-native';
 
 const Task = (props) => {
- 
+
 
     const [backgroundColor, setTaskCompletedColour] = useState("#d63230");
-    const [textDecorationLine, setTaskCompletedLine] =useState("")
-    
-        return(
-        <TouchableOpacity onLongPress={() =>{
-           console.log("Long press achieved");
-           setTaskCompletedColour("green");
-           setTaskCompletedLine("line-through")
-        }} delayLongPress={300}>
-        <View style={[styles.taskContainer,{backgroundColor}]}>
-            <Text style={[styles.textStyle,{textDecorationLine}]}> {props.text}</Text>
-        </View>
+    const [textDecorationLine, setTaskCompletedLine] = useState("");
+    const [isPressed, setPress] = useState(true);
+
+
+    const checkPressed = () => {
+
+        if (isPressed == true) {
+            setPress(false);
+            console.log(isPressed);
+        } else {
+            setPress(true);
+            console.log(isPressed);
+        }
+    }
+
+    const unCompleteTask = () => {
+
+        if(isPressed == false) {
+            setTaskCompletedColour("#d63230");
+        }else{
+            setTaskCompletedColour("green");
+
+        }
+
+    }
+
+    return (
+        <TouchableOpacity onLongPress={() => {
+            checkPressed();
+           
+            setTaskCompletedLine("line-through")
+        }} delayLongPress={300} >
+            <View style={[styles.taskContainer, { backgroundColor }]}>
+                <Text style={[styles.textStyle, { textDecorationLine }]}> {props.text}</Text>
+            </View>
         </TouchableOpacity>
     )
 }
@@ -23,24 +47,24 @@ const Task = (props) => {
 const styles = StyleSheet.create({ //this is a class which is passed
 
 
-    taskContainer:{
-        padding:24,
-        borderRadius:50,
-        alignItems:'center',
-        flexDirection:'row',
-        justifyContent:'center',
-        marginTop:10,
-        marginLeft:10,
-        marginRight:10
-    },  
-    textStyle:{
-        padding:10,
-        justifyContent:'space-between',
-        color:'white',
-        fontWeight:'bold',
-        fontSize:28,
+    taskContainer: {
+        padding: 24,
+        borderRadius: 50,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10
+    },
+    textStyle: {
+        padding: 10,
+        justifyContent: 'space-between',
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 28,
         textDecorationStyle: 'double',
-        textDecorationColor:'black',
+        textDecorationColor: 'black',
 
     }
 
